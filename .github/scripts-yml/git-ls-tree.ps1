151cc29779b7df3ba0ff026b24fe04955352f5a7
ongtrieuhau <o.ngtrieuhau861@gmail.com>
2024-09-25 10:55:34 +0700

    Bổ sung git-ls-tree

--- .github/scripts-yml/git-ls-tree.ps1
+++ .github/scripts-yml/git-ls-tree.ps1
@@ -1,13 +1,15 @@
 # Hiển thị thông tin các tệp đã được commit
-Write-Host "=== Listing committed files ===" -ForegroundColor Cyan
+Write-Host "+------------------------------+" -ForegroundColor Cyan
+Write-Host "|📄Listing committed files----|" -ForegroundColor Cyan
+Write-Host "+------------------------------+" -ForegroundColor Cyan
 git ls-tree -r -t -l HEAD
-
 # Ngăn cách bằng dòng trống để dễ đọc
 Write-Host "`n"
 
 # Hiển thị các tệp chưa được theo dõi (untracked files)
-Write-Host "=== Listing untracked files ===" -ForegroundColor Yellow
+Write-Host "+------------------------------+" -ForegroundColor Yellow
+Write-Host "|🛠️Listing untracked files----|" -ForegroundColor Yellow
+Write-Host "+------------------------------+" -ForegroundColor Yellow
 git ls-files --others
-
 # Ngăn cách bằng dòng trống để dễ đọc
 Write-Host "`n"
